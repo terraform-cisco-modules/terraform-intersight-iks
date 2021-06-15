fd0abf3a9d9ac8c8b8d057b2d5957963ba7e869d
Justin Barksdale <jbarksdale@threepings.com>
2021-06-15 16:22:25 -0400

    Changed path

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,7 +5,7 @@ provider "intersight" {
 }
 
 module "terraform-intersight-iks" {
-  source = "terraform-cisco-modules/iks/intersight//modules/terraform-intersight-iks/"
+  source = "terraform-cisco-modules/iks/intersight"
   # source = "terraform-cisco-modules/iks/intersight//"
   # Infra Config Policy Information
   cluster_name = "test0614"
