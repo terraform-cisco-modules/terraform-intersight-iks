74c25b3d4b85ea687daf4c2c253e2f337d6d3e7a
Chris Gascoigne <cgascoig@cisco.com>
2022-05-13 10:35:06 +1000

    [WIP] CI testing

---
+++ terratests/basic_cluster/main.tf
@@ -0,0 +1,164 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+    intersight = {
+      source = "CiscoDevNet/intersight"
+    }
+  }
+}
+
+provider "intersight" {
+  apikey    = var.intersight_apikey
+  secretkey = var.intersight_secretkey
+  endpoint  = var.intersight_endpoint
+}
+
+module "main" {
+  source = "../.."
+
+  # Kubernetes Cluster Profile  
+  cluster = {
+    name                = var.name
+    action              = "Unassign"
+    wait_for_completion = true
+    worker_nodes        = 5
+    load_balancers      = 4
+    worker_max          = 20
+    control_nodes       = 1
+    ssh_user            = "testiksadmin"
+    ssh_public_key      = "12345"
+  }
+
+
+  # IP Pool Information 
+  ip_pool = {
+    use_existing        = false
+    name                = var.name
+    ip_starting_address = "10.1.1.10"
+    ip_pool_size        = "20"
+    ip_netmask          = "255.255.255.0"
+    ip_gateway          = "10.1.1.1"
+    dns_servers         = ["8.8.8.8", "8.8.4.4"]
+  }
+
+  # Sysconfig Policy 
+  sysconfig = {
+    use_existing = false
+    name         = var.name
+    domain_name  = "test.ciscolabs.com"
+    timezone     = "America/New_York"
+    ntp_servers  = ["pool.ntp.org"]
+    dns_servers  = ["8.8.8.8", "8.8.4.4"]
+  }
+
+  # Kubernetes Network CIDR 
+  k8s_network = {
+    use_existing = false
+    name         = var.name
+
+    pod_cidr     = "2.2.0.0/16"
+    service_cidr = "3.3.3.0/24"
+    cni          = "Calico"
+  }
+  # Version policy 
+  versionPolicy = {
+    useExisting    = false
+    policyName     = var.name
+    iksVersionName = "1.20.14-iks.0"
+  }
+  # Trusted Registry Policy 
+  tr_policy = {
+    use_existing = false
+    create_new   = false
+    name         = var.name
+  }
+  # Runtime Policy (To create new change "use_existing" to 'false' and set "create_new' to 'true' uncomment variables and modify them to meet your needs.)
+  # Set both variables to 'false' if this policy is not needed.
+  runtime_policy = {
+    use_existing = false
+    create_new   = false
+    name         = var.name
+    # name                 = "runtime"
+    # http_proxy_hostname  = "t"
+    # http_proxy_port      = 80
+    # http_proxy_protocol  = "http"
+    # http_proxy_username  = null
+    # http_proxy_password  = null
+    # https_proxy_hostname = "t"
+    # https_proxy_port     = 8080
+    # https_proxy_protocol = "https"
+    # https_proxy_username = null
+    # https_proxy_password = null
+  }
+
+  # Infrastructure Configuration Policy (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
+  infraConfigPolicy = {
+    use_existing       = false
+    policyName         = var.name
+    platformType       = "esxi"
+    # targetName         = "prodvcenter.ceclab.info"
+    targetName         = "10.67.17.125"
+    description        = "Infra config policy for ${var.name}"
+    interfaces         = ["vm-network-28"]
+    vcTargetName       = "prodvcenter.ceclab.info"
+    vcClusterName      = "Melb-HX-Hybrid"
+    vcDatastoreName    = "cgascoig-1"
+    vcResourcePoolName = ""
+    vcPassword         = var.vsphere_password
+  }
+
+  # Addon Profile and Policies (To create new change "createNew" to 'true' and uncomment variables and modify them to meet your needs.)
+  # This is an Optional item.  Comment or remove to not use.  Multiple addons can be configured.
+  addons = [
+    # {
+    #   createNew       = true
+    #   addonPolicyName = "smm-tf"
+    #   addonName       = "smm"
+    #   description     = "SMM Policy"
+    #   upgradeStrategy = "AlwaysReinstall"
+    #   installStrategy = "InstallOnly"
+    #   releaseVersion  = "1.7.4-cisco4-helm3"
+    #   overrides       = yamlencode({ "demoApplication" : { "enabled" : true } })
+    # },
+    # {
+    # createNew = true
+    # addonName            = "ccp-monitor"
+    # description       = "monitor Policy"
+    # # upgradeStrategy  = "AlwaysReinstall"
+    # # installStrategy  = "InstallOnly"
+    # releaseVersion = "0.2.61-helm3"
+    # # overrides = yamlencode({"demoApplication":{"enabled":true}})
+    # }
+  ]
+
+  # Worker Node Instance Type (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
+  instance_type = {
+    use_existing = false
+    name         = var.name
+    cpu          = 4
+    memory       = 16386
+    disk_size    = 40
+  }
+
+  # Organization and Tag Information
+  organization = "default"
+  tags         = []
+}
+
+variable "vsphere_password" {}
+variable "intersight_apikey" {}
+variable "intersight_secretkey" {}
+variable "intersight_endpoint" {}
+variable "name" {
+  default = "test_basic_cluster"
+}
+
+output "k8s_cluster_profile_moid" {
+  value = module.main.k8s_cluster_profile_moid
+}
+
+output "k8s_cluster_moid" {
+  value = module.main.k8s_cluster_moid
+}
---
+++ terratests/basic_cluster_test.go
@@ -0,0 +1,119 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/cgascoig/intersight-go-sdk/intersight"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicCluster(t *testing.T) {
+	// Setup Terraform options
+	uniqueId := random.UniqueId()
+	instanceName := fmt.Sprintf("test_basic_cluster-%s", uniqueId)
+
+	vars := map[string]interface{}{}
+	terraform.GetAllVariablesFromVarFile(t, "../test_creds_staging.tfvars", &vars)
+	// terraform.GetAllVariablesFromVarFile(t, "../test_creds.tfvars", &vars)
+	vars["name"] = instanceName
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./basic_cluster",
+		Vars:         vars,
+	})
+
+	client, ctx := setupIntersight(t, vars["intersight_apikey"].(string), vars["intersight_secretkey"].(string), vars["intersight_endpoint"].(string))
+
+	// Tests start here:
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	k8sClusterProfileMoid := terraform.Output(t, terraformOptions, "k8s_cluster_profile_moid")
+	assert.Regexp(t, regexp.MustCompile("^[[:xdigit:]]{24}$"), k8sClusterProfileMoid, "Cluster Profile Moid")
+
+	k8sClusterMoid := terraform.Output(t, terraformOptions, "k8s_cluster_moid")
+	assert.Regexp(t, regexp.MustCompile("^[[:xdigit:]]{24}$"), k8sClusterMoid, "Cluster Moid")
+
+	// Validate Kubernetes Cluster Profile
+	res, _, err := client.KubernetesApi.GetKubernetesClusterProfileByMoid(ctx, k8sClusterProfileMoid).Execute()
+	assert.NoError(t, err, "Getting Kubernetes Cluster Profile by Moid")
+	assert.NotNil(t, res, "Getting Kubernetes Cluster Profile by Moid")
+
+	assert.Equal(t, instanceName, *res.Name, "Check Kubernetes Cluster Profile Name")
+	assert.Equal(t, "", *res.Description, "Check Kubernetes Cluster Profile Description")
+	assert.Equal(t, "Unassign", *res.Action, "Check Kubernetes Cluster Profile Action")
+	assert.EqualValues(t, 4, *res.ManagementConfig.Get().LoadBalancerCount, "Check Kubernetes Cluster Profile Load Balancer Count")
+
+	// Validate Kubernetes Cluster
+	kcRes, _, err := client.KubernetesApi.GetKubernetesClusterByMoid(ctx, k8sClusterMoid).Execute()
+	assert.Equal(t, instanceName, *kcRes.Name, "Check Kubernetes Cluster Name")
+
+	// Validate Node Group Profiles - Control Plane
+	ngpRes, _, err := client.KubernetesApi.GetKubernetesNodeGroupProfileList(ctx).Filter(fmt.Sprintf("Name eq '%s-control_profile'", instanceName)).Execute()
+	assert.NoError(t, err, "Get Control Plane Node Group Profile")
+	assert.NotNil(t, ngpRes, "Get Control Plane Node Group Profile")
+	assert.Equal(t, 1, len(ngpRes.KubernetesNodeGroupProfileList.Results), "Get Control Plane Node Group Profile by Name should return 1 result")
+
+	ngp := ngpRes.KubernetesNodeGroupProfileList.Results[0]
+	validateNodeGroupProfile(t, ngp, "ControlPlane", 1, 1, 2, k8sClusterProfileMoid)
+
+	// Validate Node Group Profiles - Workers
+	ngpRes, _, err = client.KubernetesApi.GetKubernetesNodeGroupProfileList(ctx).Filter(fmt.Sprintf("Name eq '%s-worker_profile'", instanceName)).Execute()
+	assert.NoError(t, err, "Get Worker Node Group Profile")
+	assert.NotNil(t, ngpRes, "Get Worker Node Group Profile")
+	assert.Equal(t, 1, len(ngpRes.KubernetesNodeGroupProfileList.Results), "Get Worker Node Group Profile by Name should return 1 result")
+
+	ngp = ngpRes.KubernetesNodeGroupProfileList.Results[0]
+	validateNodeGroupProfile(t, ngp, "Worker", 5, 5, 20, k8sClusterProfileMoid)
+
+}
+
+func setupIntersight(t *testing.T, keyID, key, endpoint string) (*intersight.APIClient, context.Context) {
+	// Setup Intersight API client
+	config := intersight.NewConfiguration()
+	client := intersight.NewAPIClient(config)
+
+	// Set up the authentication configuration struct
+	authConfig := intersight.HttpSignatureAuth{
+		KeyId: keyID,
+
+		SigningScheme: intersight.HttpSigningSchemeRsaSha256,
+		SignedHeaders: []string{
+			intersight.HttpSignatureParameterRequestTarget, // The special (request-target) parameter expresses the HTTP request target.
+			"Host",   // The Host request header specifies the domain name of the server, and optionally the TCP port number.
+			"Date",   // The date and time at which the message was originated.
+			"Digest", // A cryptographic digest of the request body.
+		},
+		SigningAlgorithm: intersight.HttpSigningAlgorithmRsaPKCS1v15,
+	}
+	err := authConfig.SetPrivateKey(key)
+	assert.NoError(t, err)
+
+	// Get a context that includes the authentication config
+	ctx, err := authConfig.ContextWithValue(context.Background())
+	assert.NoError(t, err)
+	assert.NotNil(t, ctx)
+
+	ctx = context.WithValue(ctx, intersight.ContextServerVariables, map[string]string{
+		"server": endpoint,
+	})
+
+	return client, ctx
+}
+
+func validateNodeGroupProfile(t *testing.T, res intersight.KubernetesNodeGroupProfile, nodeType string, desiredNodes, minNodes, maxNodes int, clusterProfileMoid string) {
+	msg := fmt.Sprintf("Validate %s Node Group Profile", nodeType)
+
+	assert.Equal(t, nodeType, *res.NodeType, msg)
+	assert.EqualValues(t, desiredNodes, *res.Desiredsize, msg)
+	assert.EqualValues(t, minNodes, *res.Minsize, msg)
+	assert.EqualValues(t, maxNodes, *res.Maxsize, msg)
+	assert.Equal(t, clusterProfileMoid, *res.ClusterProfile.MoMoRef.Moid)
+}
